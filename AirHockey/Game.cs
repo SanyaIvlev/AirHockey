b71f11e61c279188e78108b29e329b412245f18c
Ivlev Oleksandr <81967994+SanyaIvlev@users.noreply.github.com>
2024-12-26 13:39:29 -0600

    Paddle movement

--- AirHockey/Game.cs
+++ AirHockey/Game.cs
@@ -5,14 +5,22 @@ namespace Aerohockey;
 
 public class Game
 {
-    private const int WIDTH = 1600;
-    private const int HEIGHT = 900;
+    public const int WIDTH = 900;
+    public const int HEIGHT = 1600;
+    
     private RenderWindow _window;
+
+    private Paddle _paddle1;
+    private Paddle _paddle2;
+    
     
     public void Start()
     {
         _window = new RenderWindow(new VideoMode(WIDTH, HEIGHT), "Aerohockey");
         _window.Closed += WindowClosed;
+        
+        _paddle1 = new(Keyboard.Key.A, Keyboard.Key.D, false);
+        _paddle2 = new(Keyboard.Key.W, Keyboard.Key.S, true);
         
         Run();
     }
@@ -33,16 +41,27 @@ public class Game
     private void ProcessInput()
     {
         _window.DispatchEvents();
+        
+        _paddle1.ProcessInput();
+        _paddle2.ProcessInput();
     }
     
     private void Update()
     {
-        
+        _paddle1.DoLogic();
+        _paddle2.DoLogic();
     }
 
     private void Render()
     {
         _window.Clear(Color.Black);
+
+        Shape firstPaddle = _paddle1.Shape;
+        Shape secondPaddle = _paddle2.Shape;
+        
+        _window.Draw(firstPaddle);
+        _window.Draw(secondPaddle);
+        
         _window.Display();
     }
     
--- AirHockey/Paddle.cs
+++ AirHockey/Paddle.cs
@@ -1,6 +1,60 @@
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
 namespace Aerohockey;
 
 public class Paddle
 {
+    public RectangleShape Shape; 
     
+    private float _direction;
+    
+    private Keyboard.Key _leftMovementButton; 
+    private Keyboard.Key _rightMovementButton; 
+    
+    public Paddle(Keyboard.Key leftMovement, Keyboard.Key rightMovement, bool isOnTop)
+    {
+        _leftMovementButton = leftMovement;
+        _rightMovementButton = rightMovement;
+
+        Shape = new(new Vector2f(100, 30));
+        Shape.FillColor = Color.Red;
+
+        int y;
+        
+        if (isOnTop)
+            y = 200; 
+        else
+            y = Game.HEIGHT - 200;
+        
+        Shape.Position = new Vector2f(Game.WIDTH / 2, y);
+    }
+    
+    public void ProcessInput()
+    {
+        _direction = 0;
+        
+        if (Keyboard.IsKeyPressed(_leftMovementButton))
+        {
+            _direction = -1;
+        }
+        else if (Keyboard.IsKeyPressed(_rightMovementButton))
+        {
+            _direction = 1;
+        }
+    }
+
+    public void DoLogic()
+    {
+        Vector2f movementDistance = new Vector2f(_direction, 0);
+
+        float halfOfPaddle = Shape.Size.X / 2;
+        float nextCenterPositionX = Shape.Position.X + movementDistance.X + halfOfPaddle;
+
+        if (nextCenterPositionX + halfOfPaddle >= Game.WIDTH || nextCenterPositionX - halfOfPaddle <= 0)
+            return;
+        
+        Shape.Position += movementDistance;
+    }
 }
